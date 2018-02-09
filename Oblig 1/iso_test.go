96dc731dfbfae0a374b1e685b2d95b8ee79d41d0
Siris98 <35685089+Siris98@users.noreply.github.com>
2018-02-09 13:54:33 +0100

    Add files via upload

--- Oblig 1/iso_test.go
+++ Oblig 1/iso_test.go
@@ -3,18 +3,14 @@ package main
 import (
 	"testing"
 	"fmt"
-	"strconv"
+	//"strconv"
 )
 
 func TestExtendedASCIIText(t *testing.T) {
-
-	a := fmt.Sprintf("%d", ascii)
-	i, err := strconv.Atoi(a)
-
-	if i <  {
-		fmt.Println("ERROR, value not in extended ASCII")
+	for i := 0; i<len(ascii);i++ {
+		if (ascii[i] < 128) {
+			t.Fail()
+			fmt.Println("ERROR, value not in extended ASCII")
+		}
 	}
-	/*if !reflect.DeepEqual(values, expected) {
-		t.Fatalf("expected %d, actual is %d", 1, values[0])
-	}*/
 }

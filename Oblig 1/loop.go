1db62cd25771aeaeb5850127820aa2f31c84a4f0
Jarlej13 <35611995+Jarlej13@users.noreply.github.com>
2018-02-12 10:44:05 +0100

    Update loop.go

--- Oblig 1/loop.go
+++ Oblig 1/loop.go
@@ -6,7 +6,9 @@ import (
 "os/signal"
 
 )
-// Prossessen bruker 50% av prosessor og 53% av minne på en datamaskin.  Avslutningsmeldinger som programmet håndterer og godkjenner er følgende: Ctrl-C og Ctrl-break
+// Prossessen bruker ca 50% av prosessor og 12% av minne på en datamaskin. 30% prosessor og 16% på en annen.
+// Minne er uforandret om man kjører programmet eller ikke. 
+//Avslutningsmeldinger som programmet håndterer og godkjenner er følgende: Ctrl-C og Ctrl-break
 func main() {
 	d := make(chan os.Signal, 2)
 	signal.Notify(d, os.Interrupt,)

d667a89733aa5dd9e494d3613cc0d445e95c8ecd
Master programmer <35686045+N0RdmannFraN0Rge@users.noreply.github.com>
2018-03-07 13:19:54 +0100

    Update addup.go

--- Oblig 2/src/oppgave 3/addup.go
+++ Oblig 2/src/oppgave 3/addup.go
@@ -1,1 +1,54 @@
-package oppgave_3
+package main
+
+import (
+	"os"
+	"strconv"
+	"fmt"
+	"log"
+	"os/signal"
+)
+
+func main() {
+	d := make(chan os.Signal, 2)
+	signal.Notify(d, os.Interrupt,)
+	go func() {
+		<-d
+		fmt.Println("Prosess avsluttet")
+		os.Exit(1)
+	}()
+
+	functionA()
+}
+
+func functionA() {
+	input1 := os.Args[1]
+	input2 := os.Args[2]
+
+	tall1, err := strconv.Atoi(input1)
+	//Her kommer en feilmelding dersom det første argumentet ikke er et tall.
+	//Feilmeldingen sier "Invalid syntax"
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tall2, err := strconv.Atoi(input2)
+	//Her kommer en feilmelding dersom det andre argumentet ikke er et tall.
+	//Feilmeldingen sier "Invalid syntax"
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := make(chan int)
+
+	go functionB(tall1, tall2, c)
+
+	resultat := <-c
+
+	fmt.Println(resultat)
+}
+
+func functionB(tall1 int, tall2 int, c chan int) {
+	resultat := tall1 + tall2
+
+	c <- resultat
+}

9a6e61725cdf66b92818dcc45be50099e3adbe49
Siris98 <35685089+Siris98@users.noreply.github.com>
2018-04-09 10:31:38 +0200

    Add files via upload

---
+++ Oblig3/src/httpserver.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"html"
+	"log"
+)
+
+func main(){
+	http.Handle("/foo", fooHandler)
+
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}

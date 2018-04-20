705029c877588247692af990b0e39b9ab55e63c4
Jarlej13 <35611995+Jarlej13@users.noreply.github.com>
2018-04-20 14:25:59 +0200

    Create Oppg1.go

---
+++ Oblig3/src/Oppgave 1/Oppg1.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, client")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+
+
